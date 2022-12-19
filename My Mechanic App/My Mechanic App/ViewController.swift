dbf792c3b0bf9e5449e8c8d50c1fa51e8b6dc3f2
FaisalAli029 <201900929@student.polytechnic.bh>
2022-12-19 08:43:18 -0800

    Project Setup

---
+++ My Mechanic App/My Mechanic App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Mechanic App
+//
+//  Created by MacOS on 12/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ My Mechanic App/My Mechanic AppUITests/My_Mechanic_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  My_Mechanic_AppUITestsLaunchTests.swift
+//  My Mechanic AppUITests
+//
+//  Created by MacOS on 12/19/22.
+//
+
+import XCTest
+
+final class My_Mechanic_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
