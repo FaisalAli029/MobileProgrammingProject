d43a2e26a5d677c57dc464dbaa6890fc344c5309
Mask619916 <121339077+mask619916@users.noreply.github.com>
2023-01-06 04:59:14 +0300

    Added the important models [CAR and SERVICE]. Added a list view (table view) that contains some fake data for now. The file 'MyCarsMainScreenViewController' controls the home screen.

---
+++ My Mechanic App/My Mechanic App/Controllers/MyCars Screen Controllers/MyCarsMainScreenViewController.swift
@@ -0,0 +1,76 @@
+import UIKit
+
+class MyCarsMainScreenViewController: UIViewController {
+
+    @IBOutlet weak var myCarsTableView: UITableView!
+    
+    @IBAction func editBtn(_ sender: UIBarButtonItem) {
+        myCarsTableView.isEditing = !myCarsTableView.isEditing
+        sender.title = myCarsTableView.isEditing ? "Done" : "Edit"
+    }
+    
+    // List to display 'MyCars'
+    var myCarsList = carsData
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let nib = UINib(nibName: "MyCarTableViewCell", bundle: nil)
+        myCarsTableView.register(nib, forCellReuseIdentifier: "carCell")
+        myCarsTableView.delegate = self
+        myCarsTableView.dataSource = self
+    }
+}
+
+extension MyCarsMainScreenViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    // Total number of rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        myCarsList.count
+    }
+    
+    // Shows cell data and removes other cells from memory if they are NOT visible
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = myCarsTableView.dequeueReusableCell(
+            withIdentifier: "carCell",
+            for: indexPath
+        ) as! MyCarTableViewCell
+        
+        cell.manufacturerLabel.text = myCarsList[indexPath.row].manufacturer
+        cell.modelTitleLabel.text = "Model:"
+        cell.modelLabel.text = myCarsList[indexPath.row].model
+        cell.licensePlateTitleLabel.text = "License Plate:"
+        cell.licensePlateLabel.text = myCarsList[indexPath.row].licensePlate
+        
+        return cell
+    }
+    
+    // On Cell Selection
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("you have selected a cell. index = \(indexPath.row)")
+    }
+    
+    // Rows can be edited
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    // Order rows
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let tempData = myCarsList[sourceIndexPath.item]
+        myCarsList.remove(at: sourceIndexPath.item)
+        myCarsList.insert(tempData, at: destinationIndexPath.item)
+    }
+    
+    // Delete or insert data
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            myCarsList.remove(at: indexPath.row)
+            myCarsTableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // do nothing
+        }
+    }
+}
---
+++ My Mechanic App/My Mechanic App/Models/Car.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+struct Car {
+    private let dateFormatter = DateFormatter()
+    
+    let ID = UUID()
+    var manufacturer: String
+    var model: String
+    var yearManufactured: Date
+    var engine: String
+    var licensePlate: String
+    var mileage: Double
+    var cost: Double
+    var servicesList: [Service]
+    
+    func getYearManufaturedAsString() -> String {
+        return dateFormatter.string(from: self.yearManufactured)
+    }
+}
