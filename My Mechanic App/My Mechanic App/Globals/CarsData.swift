b5fb3b7683148ee115ec1207ef7963a09873c5a2
OmarAlshams <121338898+OmarAlshams@users.noreply.github.com>
2023-01-08 19:15:04 +0300

    Revert "Merge branch 'main' into omar"
    This reverts commit 20e414546fdb5710ca9d006ce339a2d60c96b999, reversing
    changes made to 01c7c5de873d5f9c0a7664afbdb9a5a2eccf190f.

--- My Mechanic App/My Mechanic App/Globals/CarsData.swift
+++ My Mechanic App/My Mechanic App/Globals/CarsData.swift
@@ -2,11 +2,12 @@ import Foundation
 
 let dateFormatter = DateFormatter()
 
-var carsData: [Car] = [
+let carsData: [Car] = [
+    
     Car(
         manufacturer: "Toyota",
         model: "Corolla",
-        yearManufactured: 1966,
+        yearManufactured: dateFormatter.date(from: "1966-11-05") ?? Date.now, // uses the custom date, otherise if null, it will use current date.
         engine: "V6",
         licensePlate: "123456",
         mileage: 17263.315,
@@ -36,7 +37,7 @@ var carsData: [Car] = [
     Car(
         manufacturer: "Koenigsegg",
         model: "Jesko",
-        yearManufactured: 2023,
+        yearManufactured: dateFormatter.date(from: "2023-05-15") ?? Date.now, // uses the custom date, otherise if null, it will use current date.
         engine: "5.0 litre twin-turbo V8",
         licensePlate: "001122",
         mileage: 0.0,
@@ -66,7 +67,7 @@ var carsData: [Car] = [
     Car(
         manufacturer: "Dodge",
         model: "Charger",
-        yearManufactured: 1966,
+        yearManufactured: dateFormatter.date(from: "1966-07-21") ?? Date.now, // uses the custom date, otherise if null, it will use current date.
         engine: "5.7L HEMI® V8",
         licensePlate: "619916",
         mileage: 0.0,
