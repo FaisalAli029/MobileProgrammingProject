8cf7fa2523a35c98a75cc93ab756bba2f73190b1
Hassan Kaz <201902669@student.polytechnic.bh>
2023-01-13 21:10:12 +0300

    login screen design finished

--- My Mechanic App/My Mechanic App/Controllers/Login and Registration Screens/LoginScreenViewController.swift
+++ My Mechanic App/My Mechanic App/Controllers/Login and Registration Screens/LoginScreenViewController.swift
@@ -1,8 +1,11 @@
 import UIKit
 
-class LoginScreenViewController: UIViewController {
+class LoginScreenViewController: UIViewController, UITextFieldDelegate {
     
     var isSignInSuccessful: Bool = true
+    
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -10,12 +13,22 @@ class LoginScreenViewController: UIViewController {
         // Do any additional setup after loading the view.
         //self.navigationItem.setHidesBackButton(true, animated: true)
         self.navigationController?.navigationBar.isHidden = true
+        
+        emailTF.delegate = self
+        passwordTF.delegate = self
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let vc = segue.destination as? MainScreenViewController {
+            // TODO: check user existence is storage
             vc.isLoggedIn = isSignInSuccessful
         }
+    }
+    
+    // To dismiss keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
 }
